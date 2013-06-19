41dc79fbf1a7fcc1f19a692a4a28a30c20290827
Jonathan Lai <jonathanlai@eltux.net>
2013-06-18 22:42:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPg::Application.config.secret_token = 'bf72eb8ef8329323d70dc8e7e4c232844a65f27a80bfd9e68f893185f7763586f7bfb62f271101faf2f181b395f306850b5ff70695e59a3a14d8b72881dcc63c'
